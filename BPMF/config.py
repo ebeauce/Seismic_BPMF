5a10850e6085ad9ff6e0f0fa5bf422efb8956455
ebeauce <ebeauce@mit.edu>
2021-06-25 19:06:45 -0400

    Start cleaning up

--- BPMF/config.py
+++ BPMF/config.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import inspect
 import numpy as np
 
@@ -26,36 +27,42 @@ class Config():
 package = os.path.dirname(inspect.getfile(inspect.currentframe())) + '/'
 # read in study parameters
 parameters_path = os.path.join(os.getcwd(), 'parameters.cfg')
-with open(parameters_path) as file:
-    param_dict = {}
-    for line in file:
-        key, value = line.split('=')
-        key = key.strip()
-        value = value.strip()
-        try:
-            value = np.float32(value)
-        except Exception:
-            pass
-        if isinstance(value, str) and len(value.split(',')) > 1:
-            value = [np.float32(freq) for freq in value.split(',')]
-        param_dict[key] = value
+if os.path.isfile(parameters_path):
+    with open(parameters_path) as file:
+        param_dict = {}
+        for line in file:
+            key, value = line.split('=')
+            key = key.strip()
+            value = value.strip()
+            try:
+                value = np.float32(value)
+            except Exception:
+                pass
+            if isinstance(value, str) and len(value.split(',')) > 1:
+                value = [np.float32(freq) for freq in value.split(',')]
+            param_dict[key] = value
+    cfg = Config(param_dict)
+else:
+    print(f'Couldn\'t find the parameter file at {parameters_path}.')
+    print('See https://github.com/ebeauce/Seismic_BPMF/ for an example'
+          ' of a parameters.cfg file.')
+    sys.exit()
 
-# specific parameter config
-# frequency bands
-if 'min_freq' and 'max_freq' in param_dict:
-    param_dict['freq_bands'] = []
-    if isinstance(param_dict['min_freq'], list):
-        n_freqs = len(param_dict['min_freq'])
-        min_freq = param_dict.pop('min_freq')
-        max_freq = param_dict.pop('max_freq')
-        for f in range(n_freqs):
-            param_dict['freq_bands'].append([min_freq[f], max_freq[f]])
-    else:
-        param_dict['freq_bands'].append(
-            [param_dict['min_freq'], param_dict['max_freq']])
+## specific parameter config
+## frequency bands
+#if 'min_freq' and 'max_freq' in param_dict:
+#    param_dict['freq_bands'] = []
+#    if isinstance(param_dict['min_freq'], list):
+#        n_freqs = len(param_dict['min_freq'])
+#        min_freq = param_dict.pop('min_freq')
+#        max_freq = param_dict.pop('max_freq')
+#        for f in range(n_freqs):
+#            param_dict['freq_bands'].append([min_freq[f], max_freq[f]])
+#    else:
+#        param_dict['freq_bands'].append(
+#            [param_dict['min_freq'], param_dict['max_freq']])
+#
+## sampling_rate (needs to be an int)
+#if 'sampling_rate' in param_dict:
+#    param_dict['sampling_rate'] = np.int32(param_dict['sampling_rate'])
 
-# sampling_rate (needs to be an int)
-if 'sampling_rate' in param_dict:
-    param_dict['sampling_rate'] = np.int32(param_dict['sampling_rate'])
-
-cfg = Config(param_dict)
