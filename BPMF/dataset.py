Fix length of normalization window in pick_PS_phases

BPMF/dataset.py
@@ -1583,7 +1583,12 @@ class Event(object):
             # momentarily update samping_rate
             sampling_rate0 = float(self.sampling_rate)
             self.sampling_rate = self.sr * upsampling / downsampling
-        data_arr_n = utils.normalize_batch(data_arr)
+        data_arr_n = utils.normalize_batch(
+                data_arr, 
+                normalization_window_sample=kwargs.get(
+                    "normalization_window_sample", data_arr.shape[-1]
+                    )
+                )
         closest_pow2 = int(np.log2(data_arr_n.shape[-1])) + 1
         diff = 2**closest_pow2 - data_arr_n.shape[-1]
         left = diff//2
@@ -1930,8 +1935,11 @@ class Event(object):
             norm[norm == 0.0] = 1.0
             data_arr /= norm
             waveform_features = envelope(data_arr)
+        self.waveform_features = waveform_features
         # print(waveform_features)
-        beamformer.backproject(waveform_features, device=device, reduce="none")
+        beamformer.backproject(
+                waveform_features, device=device, reduce="none",
+                )
         # find where the maximum focusing occurred
         src_idx, time_idx = np.unravel_index(
             beamformer.beam.argmax(), beamformer.beam.shape

BPMF/template_search.py
@@ -332,7 +332,12 @@ class Beamformer(object):
         likelihood = np.clip(likelihood, a_min=0., a_max=1.)
         return likelihood
 
-    def backproject(self, waveform_features, reduce="max", device="cpu"):
+    def backproject(
+            self,
+            waveform_features,
+            reduce="max",
+            device="cpu"
+            ):
         """Backproject the waveform features.
 
         Parameters

BPMF/utils.py
@@ -1782,7 +1782,7 @@ def read_write_waiting_list(func, path, unit_wait_time=0.2):
 # =======================================================
 
 
-def normalize_batch(seismogram, normalization_window_sample=3000):
+def normalize_batch(seismogram, normalization_window_sample=3000, overlap=0.50):
     """Apply Z-score normalization in running windows.
 
     Following Zhu et al. 2019, this function applied Z-score
@@ -1804,7 +1804,8 @@ def normalize_batch(seismogram, normalization_window_sample=3000):
     """
     from scipy.interpolate import interp1d
 
-    shift = normalization_window_sample // 2
+    #shift = normalization_window_sample // 2
+    shift = int((1. - overlap) * normalization_window_sample)
     num_stations, num_channels, num_time_samples = seismogram.shape
 
     # std in sliding windows